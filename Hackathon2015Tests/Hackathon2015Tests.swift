2c6240c2b36933611d2443a66687de2f0a3a254e
mikekavouras <kavourasm@gmail.com>
2015-10-30 17:15:27 -0400

    first

---
+++ Hackathon2015Tests/Hackathon2015Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Hackathon2015Tests.swift
+//  Hackathon2015Tests
+//
+//  Created by Michael Kavouras on 10/30/15.
+//  Copyright © 2015 Michael Kavouras. All rights reserved.
+//
+
+import XCTest
+@testable import Hackathon2015
+
+class Hackathon2015Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
